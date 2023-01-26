3bfd88a5432da51ef8231a4762bc7a65e8cf2a03
Jimmy Blanck <blanckjm@gmail.com>
2023-01-25 20:08:41 -0500

    fixed contact form

--- js/events.js
+++ js/events.js
@@ -8,48 +8,18 @@ var menuOpen3 = false;
 
 history.scrollRestoration = 'manual'; /*reset scroll position on reload*/
 
-
-
 function email() {
 	if (document.body.querySelector('.message').value != '') {
 		var templateParams = {
 		    name: document.querySelector('.name-first').value + ' ' + document.querySelector('.name-last').value,
 		    message: document.querySelector('.message').value + ' ' + document.querySelector('.phone-number').value
 		};
-		emailjs.send('service_wraq17q', 'template_37qdzrg', templateParams)
+		emailjs.send('service_8o5zc8r', 'template_9q4e9xg', templateParams)
     	.then(
           alert('Your message has been sent. Thank you! 😄')
         );
     }
     else {
         alert('Please type a message before clicking send! 😨');
-    }
-}
-
-function dropdown1() {
-    var dropdown = document.getElementById('dropdown1');
-    var menu = document.getElementById('menu1')
-    menuOpen1 = !menuOpen1;
-    if (menuOpen1){
-        dropdown.innerHTML = "-";
-        gsap.to(menu, {height: "auto", opacity: "100%", duration: 0.5});
-    }
-    else {
-        dropdown.innerHTML = "+";
-        gsap.to(menu, {height: "0px", opacity: "0%", duration: 0.5});
-    }
-}
-
-function dropdown2() {
-    var dropdown = document.getElementById('dropdown2');
-    var menu = document.getElementById('menu2');
-    menuOpen2 = !menuOpen2;
-    if (menuOpen2){
-        dropdown.innerHTML = "-";
-        gsap.to(menu, {height: "auto", opacity: "100%", duration: 0.5});
-    }
-    else {
-        dropdown.innerHTML = "+";
-        gsap.to(menu, {height: "0px", opacity: "0%", duration: 0.5});
     }
 }
