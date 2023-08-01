511504431baf2d8c293431065a7406f5b27b417d
Jimmy Blanck <blanckjm@gmail.com>
2023-08-01 10:58:33 -0400

    update fit script

--- stellar/fit_to_frame.jsx
+++ stellar/fit_to_frame.jsx
@@ -2,7 +2,7 @@ var key;
 var interval = 0.5;
 
 try{
-    var key = arguments[0];
+    var key = arguments[i];
 }catch(e){}
 
 function main() {
@@ -13,7 +13,7 @@ function main() {
     // var canceled = false;
 
     // if(key == 'stellar') {
-    //     fill(app.selection[0]);
+    //     fill(app.selection[i]);
     // }
     // else {
     //     alert('This automation only works through Stellar. Please launch it again from the app.');
@@ -26,10 +26,14 @@ function fill(frame) {
         alert('Please select a text or image frame.');
     }
     else if (frame.constructor.name == "TextFrame") {
-        do {
-            frame.paragraphs[0].pointSize = frame.paragraphs[0].pointSize + interval;
-            frame.paragraphs[0].leading = frame.paragraphs[0].pointSize * 1;
-        } while (frame.overflows == false);
+        while (frame.overflows == false) {
+            for(var i = 0;i<frame.paragraphs.length;++i) {
+                frame.paragraphs[i].pointSize = frame.paragraphs[i].pointSize + interval;
+                frame.paragraphs[i].leading = frame.paragraphs[i].pointSize * 1;
+            }
+        }
+
+        
     
         shrink(frame);
     }
@@ -38,11 +42,19 @@ function fill(frame) {
     }
 }
 
-function shrink(myTextFrame) {
-	do {
-		myTextFrame.paragraphs[0].pointSize = myTextFrame.paragraphs[0].pointSize - interval;
-		myTextFrame.paragraphs[0].leading = myTextFrame.paragraphs[0].pointSize * 1;
-	} while (myTextFrame.overflows == true);
+function shrink(frame) {
+	while (frame.overflows == true) {
+		for(var i = 0;i<frame.paragraphs.length;++i) {
+            frame.paragraphs[i].pointSize = frame.paragraphs[i].pointSize - interval;
+            frame.paragraphs[i].leading = frame.paragraphs[i].pointSize * 1;
+        }
+	}
+
+    var i = frame.paragraphs.length - 1;
+    if(i > 0) {
+        frame.paragraphs[i].pointSize = frame.paragraphs[i-1].pointSize;
+        frame.paragraphs[i].leading = frame.paragraphs[i-1].leading;
+    }
 }
 
 // main();
