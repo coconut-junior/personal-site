f8dbbb3abdaac8e730c29d4069a4b18d1ca81716
Jimmy Blanck <blanckjm@gmail.com>
2025-05-07 10:37:04 -0400

    updated status text

--- stellar/search_for_links.jsx
+++ stellar/search_for_links.jsx
@@ -125,12 +125,13 @@ for (i = 0; i < imgs.length; i++) {
     var corpFolder = Folder(getCorpFolder(Folder(spFolder))).fsName;
     var combinedPath;
 
+    status.text = 'Searching for ' + relBrokenPath + '...';
+
     if (img.linkResourceURI.match('Designs')) {
       combinedPath = designFolder + relBrokenPath;
     } else if (
       img.linkResourceURI.replaceAll('%20', ' ').match('Corporate Assets')
     ) {
-      status.text = 'Searching for ' + relBrokenPath + '...';
       combinedPath = corpFolder + relBrokenPath;
     }
     //replace slashes with correct one for mac or windows
