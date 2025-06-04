847442a10411821f7861ce05d89896a28543e728
Jimmy Blanck <blanckjm@gmail.com>
2025-06-04 16:26:53 -0400

    updated export group script

--- stellar/export_groups.jsx
+++ stellar/export_groups.jsx
@@ -1,32 +1,62 @@
 //backport replaceall function to es3
-String.prototype.replaceAll = function(str1, str2, ignore) 
-{
-    return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
-}
+String.prototype.replaceAll = function (str1, str2, ignore) {
+  return this.replace(
+    new RegExp(
+      str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g, '\\$&'),
+      ignore ? 'gi' : 'g'
+    ),
+    typeof str2 == 'string' ? str2.replace(/\$/g, '$$$$') : str2
+  );
+};
+
+app.jpegExportPreferences.exportResolution = 72;
+app.jpegExportPreferences.jpegQuality = JPEGOptionsQuality.LOW;
 
 var doc = app.activeDocument;
 var groups = doc.groups;
-var path = doc.fullName.fsName.replace(doc.fullName.fsName.split('/')[doc.fullName.fsName.split('/').length-1],'');
-var imgPath = Folder(path + "/images");
+var path = doc.fullName.fsName.replace(
+  doc.fullName.fsName.split('/')[doc.fullName.fsName.split('/').length - 1],
+  ''
+);
+var imgPath = Folder(path + '/images');
 
-if(doc.saved) {
-    if(!imgPath.exists) {
-        imgPath.create();
-    }
-    exportAll();
+if (doc.saved) {
+  if (!imgPath.exists) {
+    imgPath.create();
+  }
+  exportAll();
+} else {
+  alert('Please save your document first!');
 }
-else {
-    alert('Please save your document first!');
-}
-
 
 function exportAll() {
-    for(var i = 0;i<groups.length;++i) {
-        var g = groups[i];
-        var pngName = g.textFrames[g.textFrames.length-1].contents.replace(/([^0-9a-z])/ig, "");
-        if(pngName.length > 16) {pngName = pngName.slice(0,16);}
+  for (var i = 0; i < groups.length; ++i) {
+    var g = groups[i];
+    var fileName = undefined;
 
-        g.exportFile(format = ExportFormat.PNG_FORMAT, to = path + "/images/" + pngName + ".png");
+    for (var j = 0; j < g.textFrames.length; ++j) {
+      var t = g.textFrames[j];
+
+      for (var k = 0; k < t.paragraphs.length; ++k) {
+        var para = t.paragraphs[k];
+        if (para.appliedParagraphStyle.name.match('mainline')) {
+          fileName = para.contents
+            .toLowerCase()
+            .replace(/([^0-9a-z])/gi, '_')
+            .replaceAll('__', '_');
+        }
+      }
     }
-    alert('Complete! \nYour images can be found in ' + path + "/images");
+
+    if (fileName != undefined) {
+      if (fileName.length > 16) {
+        fileName = fileName.slice(0, 16);
+      }
+      g.exportFile(
+        (format = ExportFormat.JPG),
+        (to = path + '/images/' + fileName + '.jpg')
+      );
+    }
+  }
+  alert('Complete! \nYour images can be found in ' + path + '/images');
 }
